ce7b67dbbefcf40abb93593473ec230537163a45
Tatiana Churkina <tatiana.churkina@stacksports.com>
2022-06-12 14:50:13 +0300

    feat: добавлено видео любимовки

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,9 @@
-const videoPlayButton = document.querySelector('.performance__play');
-const video = document.querySelector('.performance__video');
+const videoPlayButton = document.querySelector('.play');
+const video = document.querySelector('.description__video');
 let videoIsPlayed = false;
 const videoPlayButtonClick = (event) => {
-    if (!videoIsPlayed) {
-        video.play();
-        videoIsPlayed = true;
-    } else {
-        video.pause();
-        videoIsPlayed = false;
-    }
+    video.setAttribute('controls', 'controls');
+    videoPlayButton.classList.add('play_type_hidden');
+    video.play();
 }
 videoPlayButton.addEventListener('click', videoPlayButtonClick);
